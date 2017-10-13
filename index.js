1a10f11a35f0562436bd1f7c48921b8ac954dc19
zyevegor <zyev.egor@gmail.com>
2017-10-13 11:51:36 +0300

    https://chronobank.atlassian.net/browse/MINT-535 - eth middleware bugs and features for next sprint

--- index.js
+++ index.js
@@ -17,27 +17,42 @@ mongoose.Promise = Promise;
 mongoose.connect(config.mongo.uri, {useMongoClient: true});
 
 let init = async () => {
-  let conn = await amqp.connect(config.rabbit.url);
+  let conn = await amqp.connect(config.rabbit.url)
+    .catch(() => {
+      log.error('rabbitmq is not available!');
+      process.exit(0);
+    });
+
   let channel = await conn.createChannel();
+
+  channel.on('close', () => {
+    log.error('rabbitmq process has finished!');
+    process.exit(0);
+  });
 
   let provider = new Web3.providers.IpcProvider(config.web3.uri, net);
   const web3 = new Web3();
   web3.setProvider(provider);
 
-  try {
-    await channel.assertExchange('events', 'topic', {durable: false});
-    await channel.assertQueue(`app_${config.rabbit.serviceName}.balance_processor`);
-    await channel.bindQueue(`app_${config.rabbit.serviceName}.balance_processor`, 'events', `${config.rabbit.serviceName}_transaction.*`);
-  } catch (e) {
-    log.error(e);
-    channel = await conn.createChannel();
-  }
+  web3.currentProvider.connection.on('end', () => {
+    log.error('ipc process has finished!');
+    process.exit(0);
+  });
 
+  web3.currentProvider.connection.on('error', () => {
+    log.error('ipc process has finished!');
+    process.exit(0);
+  });
+
+  await channel.assertExchange('events', 'topic', {durable: false});
+  await channel.assertQueue(`app_${config.rabbit.serviceName}.balance_processor`);
+  await channel.bindQueue(`app_${config.rabbit.serviceName}.balance_processor`, 'events', `${config.rabbit.serviceName}_transaction.*`);
   channel.prefetch(2);
+
   channel.consume(`app_${config.rabbit.serviceName}.balance_processor`, async (data) => {
     try {
-      let blockHash = JSON.parse(data.content.toString());
-      let tx = await Promise.promisify(web3.eth.getTransaction)(blockHash);
+      let block = JSON.parse(data.content.toString());
+      let tx = await Promise.promisify(web3.eth.getTransaction)(block.hash || '');
 
       let accounts = tx ? await accountModel.find({address: {$in: [tx.to, tx.from]}}) : [];
 
@@ -55,6 +70,7 @@ let init = async () => {
       }
 
     } catch (e) {
+      console.log(e)
       log.error(e);
     }
 
