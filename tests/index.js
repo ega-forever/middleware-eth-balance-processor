960125f06caa1229364a00ae2a1dff10cce6cae0
kirya <cloudkserg11@gmail.com>
2018-06-14 01:23:47 +0300

    https://chronobank.atlassian.net/browse/MD-391 - middleware eth - balance processor, update balance by user created in rest

--- tests/index.js
+++ tests/index.js
@@ -29,15 +29,16 @@ const awaitLastBlock = require('./helpers/awaitLastBlock'),
 describe('core/balance processor', function () {
 
   before(async () => {
+    await accountModel.remove();
     let provider = new Web3.providers.IpcProvider(config.web3.uri, net);
     web3.setProvider(provider);
 
     return await awaitLastBlock(web3);
   });
 
-  after(() => {
+  after(async () => {
     web3.currentProvider.connection.end();
-    return mongoose.disconnect();
+    return await mongoose.disconnect();
   });
 
   it('add account (if not exist) to mongo', async () => {
@@ -88,6 +89,33 @@ describe('core/balance processor', function () {
       })()
     ]);
 
+  });
+
+  it('refresh account in mongo', async () => {
+      await accountModel.remove();
+      await new accountModel({address: accounts[0]}).save();
+  });
+
+  it('send message about new account and check this balance', async () => {
+    let account = await accountModel.findOne({address: accounts[0]});
+    expect(account.balance.confirmed.toNumber()).to.be.equal(0);
+    expect(account.balance.unconfirmed.toNumber()).to.be.equal(0);
+    expect(account.balance.vested.toNumber()).to.be.equal(0);
+
+    const channel = await amqpInstance.createChannel(); 
+    await channel.assertExchange('internal', 'topic', {durable: false});
+    await channel.publish('internal', `${config.rabbit.serviceName}_user.created`, 
+      new Buffer(JSON.stringify({
+        address: accounts[0]
+      }))
+    );
+    await Promise.delay(4000);
+    account = await accountModel.findOne({address: accounts[0]});
+
+    expect(account.balance.confirmed.toNumber()).to.be.not.equal(0);
+    expect(account.balance.unconfirmed.toNumber()).to.be.not.equal(0);
+    expect(account.balance.vested.toNumber()).to.be.not.equal(0);
+    
   });
 
 });
