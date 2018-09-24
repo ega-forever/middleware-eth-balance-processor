b08510f1f125a6bda43ceac891ca6386b65b141d
kirya <cloudkserg11@gmail.com>
2018-09-24 17:06:58 +0300

    https://chronobank.atlassian.net/browse/MD-688 - parseInt to CHECK_SYSTEM

--- config/index.js
+++ config/index.js
@@ -34,7 +34,7 @@ const config = {
     exchange: process.env.SYSTEM_RABBIT_EXCHANGE || 'internal',
     serviceName: process.env.SYSTEM_RABBIT_SERVICE_NAME || 'system' 
   },
-  checkSystem: process.env.CHECK_SYSTEM || true,
+  checkSystem: process.env.CHECK_SYSTEM ? parseInt(process.env.CHECK_SYSTEM) : true,
   web3: {
     network: process.env.NETWORK || 'development',
     providers: _.chain(process.env.PROVIDERS).split(',')
